ee171d1b3469c9d7b09df00d54176b57fd3daac7
Aashirya <ak1107@wildcats.unh.edu>
2017-04-19 11:34:20 -0500

    making changes

--- unh698_test.py
+++ unh698_test.py
@@ -21,7 +21,13 @@ class FlaskrTestCase(unittest.TestCase):
         rv = self.app.get('/')  
         # Search the page contents for the link to your topic page 
         # Replace xxxxxxxxxxxx with text you'd expect to see on your main page that links to your subpage
-        assert b'./main' in rv.data 
+        assert b'./main' in rv.data
+
+    def test_my_topic(self):
+        # Replace '/' with the page path you want to make
+        rv = self.app.get('/new_page')  
+        # Replace UNH698 Website with the text you expect to see on you topic page
+        assert b'hi all, welcome!!!!!' in rv.data 
 
 if __name__ == '__main__':
     unittest.main()
